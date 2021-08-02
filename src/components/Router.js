eb1768f5e39c2225b2611fca546da01bfa8cfa7d
문규찬 <lazychan90@gmail.com>
2021-08-02 15:30:32 +0900

    firebase에 유저 정보 저장하기

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
-import Home from "../routes/Home";
-import Auth from "../routes/Auth";
+import Home from "routes/Home";
+import Auth from "routes/Auth";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <HashRouter>
       <Switch>
         {isLoggedIn ? (
           <>
             <Route>
-              <Home />
+              <Home exact path="/" />
             </Route>
           </>
         ) : (
           <>
             <Route>
-              <Auth />
+              <Auth exact path="/" />
             </Route>
           </>
         )}
