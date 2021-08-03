12a5d3aa1b8f45d17717f7ba35a09739209111e6
문규찬 <lazychan90@gmail.com>
2021-08-04 00:56:32 +0900

    firestore 생성후 데이터 보내주기

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,15 +2,21 @@ import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from "routes/Home";
 import Auth from "routes/Auth";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <HashRouter>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
-            <Route>
-              <Home exact path="/" />
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
             </Route>
           </>
         ) : (
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fbase";
 
 const Home = () => {
-  return <span>home</span>;
+  const [kyuWitt, setKyuWitt] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("kyuwitter").add({
+      kyuWitt,
+      createdAt: Date.now(),
+    });
+    setKyuWitt("");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setKyuWitt(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="what's on your mind?"
+          value={kyuWitt}
+          onChange={onChange}
+        />
+        <input type="submit" value="✅" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
