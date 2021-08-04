ea324134b6d53742a4f4d05fe20351464d8b59c8
문규찬 <lazychan90@gmail.com>
2021-08-04 17:42:46 +0900

    mention 실시간으로 가져오기 CRUD 에서 CR 완료

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [kyuWitt, setKyuWitt] = useState("");
+  const [mention, setMention] = useState([]);
+
+  // const getMention = async () => {
+  //   const dbMention = await dbService.collection("kyuwitter").get();
+  //   dbMention.forEach((document) => {
+  //     const mentionObj = {
+  //       ...document.data(),
+  //       id: document.id,
+  //     };
+  //     setMention((prev) => [mentionObj, ...prev]);
+  //   });
+  // }; !!!!!!멘션을 가져오는 구식의 방식!!!
+
+  useEffect(() => {
+    // getMention();
+    dbService.collection("kyuwitter").onSnapshot((snapshot) => {
+      const mentionArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMention(mentionArray);
+    });
+  }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("kyuwitter").add({
-      kyuWitt,
+      text: kyuWitt,
       createdAt: Date.now(),
+      creatorId: userObj.uid,
     });
     setKyuWitt("");
   };
@@ -31,6 +55,13 @@ const Home = () => {
         />
         <input type="submit" value="✅" />
       </form>
+      <div>
+        {mention.map((data) => (
+          <div key={data.id}>
+            <h4>{data.text}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
